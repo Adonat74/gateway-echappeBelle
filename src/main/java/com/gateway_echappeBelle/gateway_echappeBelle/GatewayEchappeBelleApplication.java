bce4f66b847da1d69247230d88d851bea6541290
Adonat74 <donatgoninet.antoine@gmail.com>
2024-11-15 13:50:38 +0100

    first commit

---
+++ src/main/java/com/gateway_echappeBelle/gateway_echappeBelle/GatewayEchappeBelleApplication.java
@@ -0,0 +1,13 @@
+package com.gateway_echappeBelle.gateway_echappeBelle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayEchappeBelleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayEchappeBelleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gateway_echappeBelle/gateway_echappeBelle/GatewayEchappeBelleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway_echappeBelle.gateway_echappeBelle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayEchappeBelleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
